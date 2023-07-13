f7ab1a5bf4898f94a24ee91a33af68d35419bb03
ahmedgado0o <lucian_2010@hotmail.com>
2023-07-13 20:39:11 +0000

    6th c task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main: printf
+ * main - printf
  *
  * description: prints with proper grammar, but the outcome is a piece of art
  *
- * return: always 0
+ * return: always0
  *
  */
 int main(void)
