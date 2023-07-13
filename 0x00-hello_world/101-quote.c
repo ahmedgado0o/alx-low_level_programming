b6b18fc0e1582518c6c199f5991b0d19d0937bab
ahmedgado0o <lucian_2010@hotmail.com>
2023-07-13 21:43:47 +0000

    9th c task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
  *
  * description: prints
  *
- * Return: alwyas1
+ * Return: alwyas 1 (success)
  *
  */
 int main(void)
 {
-	const char message[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	const char message[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(STDERR_FILENO, message, sizeof(message) - 1);
 	return (1);
 }
