d9efe57b5153d95da1c634f5e6bbeeafae911ab6
ahmedgado0o <lucian_2010@hotmail.com>
2023-07-13 20:28:31 +0000

    6th c task

---
+++ 0x00-hello_world/printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main: printf
+ *
+ * description: prints with proper grammar, but the outcome is a piece of art
+ *
+ * return: always 0
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
