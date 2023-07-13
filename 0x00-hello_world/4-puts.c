0d7c59ff2889a864952ea93972167e4c54de9acf
ahmedgado0o <lucian_2010@hotmail.com>
2023-07-13 19:54:40 +0000

    5th c task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,9 @@
  * description to print Programming is like building a multilingual puzzle
  */
 #include <stdio.h>
+/*
+ * main function prints Programming is like building a multilingual puzzle
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
