0533e40c3a05714e9b3f5dbf93540f147d0e533c
ahmedgado0o <lucian_2010@hotmail.com>
2023-07-13 19:58:40 +0000

    5th c task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,11 @@
  */
 #include <stdio.h>
 /*
- * main function prints Programming is like building a multilingual puzzle
+ *
+ * main-function prints Programming is like building a multilingual puzzle
+ *
+ * return:always 0 (success)
+ *
  */
 int main(void)
 {
