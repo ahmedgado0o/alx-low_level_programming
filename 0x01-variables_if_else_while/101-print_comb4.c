8a1ddae03edf95c6b6dd324ac24243f2501beff2
ahmedgado0o <lucian_2010@hotmail.com>
2023-07-14 21:21:47 +0000

    for

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,13 +13,13 @@ int main(void)
 	{
 		for (j = i + 1; j < 10; ++j)
 		{
-			for (k = i + 2; k < 10; ++k)
+			for (k = j + 1; k < 10; ++k)
 			{
-				if (i != j && i != k && j != k)
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i != 7 || j != 8 || k != 9)
 				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
 					putchar(',');
 					putchar(' ');
 				}
