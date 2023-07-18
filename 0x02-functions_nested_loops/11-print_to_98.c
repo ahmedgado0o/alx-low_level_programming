919673bee6e45bb36718b70c0aa75b11332fa32e
ahmedgado0o <lucian_2010@hotmail.com>
2023-07-18 23:11:04 +0000

    add

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,29 +8,26 @@
  */
 void print_to_98(int n)
 {
-	if (n > 98)
-	{
-		while (n != 98)
+	while (n > 98)
 		{
-			_putchar(n + '0');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
 			_putchar(',');
 			_putchar(' ');
 			n--;
 		}
-	}
-	else if (n < 98)
-	{
-		while (n != 98)
+	
+	while (n < 98)
 		{
-			_putchar(n + '0');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
 			_putchar(',');
 			_putchar(' ');
 			n++;
 		}
-	}
-	else
-	{
-		_putchar(n + '0');
-	}
+	
+		_putchar('9');
+		_putchar('8');
+		_putchar('\n');
 }
 
