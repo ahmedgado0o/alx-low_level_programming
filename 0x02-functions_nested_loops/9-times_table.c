30b6d4bf13e5fcc580dd78c0a3491ecbd066a5f4
ahmedgado0o <lucian_2010@hotmail.com>
2023-07-18 22:15:09 +0000

    for

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 				_putchar(r / 10 + '0');
 				_putchar(r % 10 + '0');
 			}
-			if (j < 9 && r < 10)
+			if (j <= 9 && r <= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
