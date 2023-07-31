72c79cc935e36729af05ac553becb1ad8db157f5
ahmedgado0o <lucian_2010@hotmail.com>
2023-07-31 11:49:49 -0700

    morre

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_chessboard - print chessboard
+ * @a: char
+ * Return:
+ */
+void print_chessboard(char (*a)[8])
+{
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			printf("%c ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
