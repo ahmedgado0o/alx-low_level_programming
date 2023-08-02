96c91e21dac8da9b9f0b2995b54ec432b5936056
ahmedgado0o <lucian_2010@hotmail.com>
2023-08-02 14:46:22 -0700

    advanced 2

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,12 +12,10 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (1);
 	}
-	if (*s1 == *s2 || *s2 == '*')
+	if (*s1 == *s2 || (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == *(s2 + 1)))
 	{
 		if (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1))
-		{
 			return (1);
-		}
 	}
 	return (0);
 }
