a154bbcf9f04d4249c0febf451f0e1e80b88f538
ahmedgado0o <lucian_2010@hotmail.com>
2023-08-02 13:01:57 -0700

    6th

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns square
+ * @n: int in
+ * Return: n or -1
+ */
+int _sqrt_recursion(int n)
+{
+	int guess;
+	int improved_guess;
+
+	guess = n / 2;
+	improved_guess = (guess + n / guess) / 2;
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	if (improved_guess == guess)
+	{
+		return (guess);
+	}
+	return (_sqrt_recursion(improved_guess));
+}
