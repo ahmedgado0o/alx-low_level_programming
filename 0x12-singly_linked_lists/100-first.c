f4afa23ced65089922b409710664b08ba6f33f77
ahmedgado0o <lucian_2010@hotmail.com>
2023-08-24 16:24:08 -0700

    ad 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * printIntro - prints
+ * Return:
+ */
+void printIntro(void)
+{
+	printf("You're beat! and yet, you must ""
+allow,\nI bore my house upon my back!\n");
+}
